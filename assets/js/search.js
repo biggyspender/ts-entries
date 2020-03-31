dc4ce68e5329afd812da6e6fa21a1ab4a346c0b5
Chris Sperry <christophermartinsperry@gmail.com>
2020-03-31 07:08:02 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"entries","url":"globals.html#entries","classes":"tsd-kind-function tsd-has-type-parameter"}]};
